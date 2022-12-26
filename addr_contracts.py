af7d9096e17cf9d8ce73fdc9fb5fc367e21afb51
webjunk4bill <bnoellert@apple.com>
2022-12-26 10:40:57 -0600

    Initial Commit.

---
+++ addr_contracts.py
@@ -0,0 +1,10 @@
+# Contract addresses
+
+elephant_farms = '0x71B00a9C9cC1902EfdDd6ba28850F6f34f5938Ed'
+ele_busd_treasury = '0xCb5a02BB3a38e92E591d323d6824586608cE8cE4'
+ele_bertha = '0xAF0980A0f52954777C491166E7F40DB2B6fBb4Fc'
+trunk_treasury = '0xaCEf13009D7E5701798a0D2c7cc7E07f6937bfDd'
+
+elephant_farms_max_apr = 1.25
+elephant_stampede_max_apr = 2.05
+drip_apr = 3.65
