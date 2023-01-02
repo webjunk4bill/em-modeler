7c92241b32f6a8062068436b6467e38f99e9e24d
webjunk4bill <bnoellert@apple.com>
2023-01-02 16:26:47 -0600

    Update BSC gas calculator to also pull out BNB.

--- utilities/bsc_gasfees.py
+++ utilities/bsc_gasfees.py
@@ -9,17 +9,18 @@ import glob
 import numpy as np
 import re
 
-path = ''  # Enter your path here
-csv_files = glob.glob(os.path.join(path, "*.csv"))
+path = '  '  # Enter your path here
+csv_files = glob.glob(os.path.join(path, "export*.csv"))
 fees = {}
 for f in csv_files:
     wallet = re.search(r"-(.*)\.csv", f).group(1)
     df = pd.read_csv(f, index_col=False)
     df['fee_usd'] = np.multiply(df['TxnFee(BNB)'], df['Historical $Price/BNB'])
-    fees[wallet] = np.sum(df['fee_usd'])
+    fees[wallet] = {'USD': np.sum(df['fee_usd']), 'BNB': np.sum(df['TxnFee(BNB)'])}
 
-df_fee = pd.Series(fees)
-total_fees = np.sum(df_fee)
-df_fee['total'] = total_fees
-df_fee.to_csv(os.path.join(path, "total_fees.csv"))
+df_fee = pd.DataFrame(fees)
+total_fees = np.sum(df_fee.iloc[0])
+total_bnb = np.sum(df_fee.iloc[1])
+df_fee['total'] = [total_fees, total_bnb]
+df_fee.T.to_csv(os.path.join(path, "total_fees.csv"))
 print(total_fees)
