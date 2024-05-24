b8ac7175c332de51abd66c3dd677d8e7e8964c0a
webjunk4bill <bnoellert@apple.com>
2024-05-24 16:48:06 -0500

    Update futures playground.

--- utilities/futures_playground.py
+++ utilities/futures_playground.py
@@ -3,17 +3,17 @@ import pandas as pd
 import pickle
 
 funds_in = 0
-group_rate = 0.00174
+group_rate = 0.95 / 365
 engine = bsc.YieldEngineV8(funds_in, group_rate)
-engine.balance = 54130
-engine.deposits = 28202
-website_claimed = 53387
+engine.balance = 100000
+engine.deposits = 50000
+website_claimed = 140000
 engine.claimed = (engine.deposits + website_claimed - engine.balance) / 2  # to get this from the website values:
 # Different from how I track
 # web: Withdrawls = (Deposits + Claimed - TVL)/2, so Compounds = Claimed - Withdrawls
 # Website shows combined claims and compounds.  A true claim is added to claimed and subtracted from balance
 engine.compounds = website_claimed - engine.claimed
-engine.daily_payout = engine.balance * group_rate
+# engine.daily_payout = engine.balance * group_rate
 data = {'claimed': [engine.claimed],
         'balance': [engine.balance],
         'deposits': [engine.deposits],
@@ -26,11 +26,10 @@ count = 0
 initial_count = 0
 deposit_after_claim = False
 
-# schedule = ['dep', 'dep', 'dep', 'dep', 'claim', 'claim', 'claim']
 schedule = {  # Start from T = 0, always deposit first, than claim, then will reset
     "initial": 0,
-    "dep": 15,
-    "claim": 25,
+    "dep": 7,
+    "claim": 12,
 }
 days = round(365 * 2)
 cycles = round(days / len(schedule)) + 1
